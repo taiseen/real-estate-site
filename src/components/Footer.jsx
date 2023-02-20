2f37c0c91890ecaaea00a735c165bb8ffd888eeb
taiseen <taiseen.cse@gmail.com>
2023-02-20 22:51:28 +0600

    fix: programmatically year add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className='bg-black/80 py-8 text-center text-white'>
-    <div className='container mx-auto'>Copyright &copy; 2023, All Right Reserved, Taiseen</div>
+    <div className='container mx-auto'>Copyright &copy; {new Date().getFullYear()}, All Right Reserved, Taiseen</div>
   </footer>
 );
 
